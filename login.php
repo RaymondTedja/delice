de352b3485c6ffb472a0c386bb649d1c1137c28b
Darren Xulouis <12116493@student.uts.edu.au>
2016-08-19 18:53:15 +1000

    Login page front end
    Finished login page front end. Can still be improved if needed. main.css
    needs improvement on naming such as ".register", ".register2", and
    ".login" - Darren , Raymond

---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Delice - Login Page </title>
+  <link href='https://fonts.googleapis.com/css?family=Merriweather|Montserrat' rel='stylesheet' type='text/css'>
+  <link href="css/main.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
+<body>
+  <div class="title2" style="padding-top:50px;padding-bottom:5px;">
+    <center> Login </center>
+  </div>
+  <form method="POST" action="">
+    <div class="formpad">
+      <input type="text" class="login" placeholder ="Username" required>
+      <br><br>
+    	<input type="password" class="login" login placeholder ="Password" required>
+      <br><br>
+      <input type="submit" class="testbtn3" value="Login">
+      <br>
+    </div>
+  </form>
+</body>
+</html>
