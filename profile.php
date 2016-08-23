874fda154f900bccace75b5e47d6b05b87689e57
Timothy alfares <k_vin92@yahoo.com>
2016-08-23 19:58:28 +1000

    add profile page
    profile page added

---
+++ profile.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title> Delice - Profile page </title>
+  <link href='https://fonts.googleapis.com/css?family=Merriweather|Montserrat' rel='stylesheet' type='text/css'>
+  <link href="css/main.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+</head>
+  <body>
+    <div class="header">        
+        <ul>
+          <li><a class="active" href="#home">Delice</a></li>
+          <li><a href="#news">News</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+      </div>
+    <div class="bgprofile">
+      <div class="padprofile">
+        <img src="img/pp.jpeg" class="pp">
+        <div class="ppname">
+          Raissa Andriana
+        </div>
+        <div class="bio">
+          Hi, my name is Raissa
+        </div>
+        <div class="biopost">
+          <div class="bios">
+            23 Stories 
+          </div>
+          <div class="bios">
+            10 Following 
+          </div>
+          <div class="bios">
+            34k Followers
+          </div>
+        </div>
+        <div style="style:clear">
+      </div>
+    </div> 
+  </body>
+</html>
